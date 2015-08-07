6e7332e1e6b5cfcc670575e4e429bfbf3d560cde
Bas Meijer <bas.meijer@me.com>
2015-08-07 14:45:43 +0200

    cross platform stuff

---
+++ roles/ansible-cygwin/files/chocolatey.ps1
@@ -0,0 +1,45 @@
+Set-ExecutionPolicy Unrestricted
+
+# Get the ID and security principal of the current user account
+$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+
+# Get the security principal for the Administrator role
+$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+
+# Check to see if we are currently running "as Administrator"
+if ($myWindowsPrincipal.IsInRole($adminRole))
+   {
+   Write-Output "chocolatey.ps1: installing a package as Administrator"
+   clear-host
+   }
+else
+   {
+   Write-Output "chocolatey.ps1 is not running as Administrator - so relaunch as administrator"
+
+   # Create a new process object that starts PowerShell
+   $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+
+   # Specify the current script path and name as a parameter
+   $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+
+   # Indicate that the process should be elevated
+   $newProcess.Verb = "runas";
+
+   # Start the new process
+   [System.Diagnostics.Process]::Start($newProcess);
+
+   # Exit from the current, unelevated, process
+   exit
+   }
+
+ $package = $args[0]
+ & elevate -c -w choco install $package -force -y
+
+
+
+
+
+
+
+
